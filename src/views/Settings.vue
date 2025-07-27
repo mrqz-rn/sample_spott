99563025424dbd4779baed088a0e50184b4082c0
Ron Marquez <75607182+mrqz-rn@users.noreply.github.com>
2025-07-27 10:05:26 +0800

    Update Settings.vue

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -6,23 +6,26 @@
         </h1>
         <div>
             <div>
-                <button @click="remove_user"
+                <button @click="confirm_remove_user"
                   class="w-full bg-green-700 text-white text-lg py-3 my-2 rounded-full hover:bg-green-800 active:scale-96 transition duration-150">
                   Remove User Session
                 </button>
-                  <button @click="remove_attlogs"
+                  <button @click="confirm_remove_attlogs"
                   class="w-full bg-blue-700 text-white text-lg py-3 my-2 rounded-full hover:bg-blue-800 active:scale-96 transition duration-150">
                   Reset Attlogs
                 </button>
             </div>
             <div class="absolute bottom-20 w-md">
-              <button @click="logout"
+              <button @click="confirm_logout"
                 class="w-full bg-rose-600 text-white text-lg py-3 my-2 rounded-full hover:bg-rose-700 active:scale-96 transition duration-150">
                 Logout
               </button>
             </div>
         </div>
       </div>
+      <ConfirmModal :options="modal_object.user" @confirm="remove_user"/>
+      <ConfirmModal :options="modal_object.attlogs" @confirm="remove_attlogs"/>
+      <ConfirmModal :options="modal_object.logout" @confirm="logout"/>
   </div>
 </template>
 
@@ -31,22 +34,55 @@ import { useRouter } from 'vue-router'
 import { useSessionStore } from '@/stores/sessionStore';
 import { loader } from '@/stores/loaderStore'
 import { alertmodal } from '@/stores/alertStore'
+import ConfirmModal from '@/components/ConfirmModal.vue';
+import { reactive } from 'vue';
 
 
 const sessionStore = useSessionStore()
 const router = useRouter()
 
 
+const modal_object = reactive({
+  user: {
+    type: 'confirm',
+    status: false,
+    message: '',
+  },
+  attlogs: {
+    type: 'confirm',
+    status: false,
+    message: '',
+  },
+  logout: {
+    type: 'confirm',
+    status: false,
+    message: '',
+  }
+})
+
+
+const confirm_remove_user = () => {
+  modal_object.user.status = true
+  modal_object.user.message = 'Are you sure you want to remove the user session?'
+}
 const remove_user = () => {
   sessionStore.removeUserSession()
   alertmodal.show('Success', 'User Session Removed')
 }
 
+const confirm_remove_attlogs = () => {
+  modal_object.attlogs.status = true
+  modal_object.attlogs.message = 'Are you sure you want to reset the attlogs?'
+}
 const remove_attlogs = () => {
   sessionStore.removeAttlogSession()
   alertmodal.show('Success', 'Attlogs Reset')
 }
 
+const confirm_logout = () => {
+  modal_object.logout.status = true
+  modal_object.logout.message = 'Are you sure you want to logout?'
+}
 const logout = () => {
   loader.show('Please wait...')
   sessionStore.logout()
